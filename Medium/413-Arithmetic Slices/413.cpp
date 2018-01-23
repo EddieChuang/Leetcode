8f60dc338b9eab0baac9d649cd9721c7df01defe
chiamin <az564123>
2018-01-23 08:38:49 +0800

    dynamic programming

---
+++ Medium/413-Arithmetic Slices/413.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        
+        int dp[10000] = {0};
+        int sum = 0;
+        for (int i = 2; i < A.size(); i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+                sum += dp[i];
+            }
+        }
+        return sum;
+    }
+};
